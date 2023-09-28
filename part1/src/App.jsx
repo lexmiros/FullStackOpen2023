8eccea4c7a769c7c58b0c6225bc15b1602d4cb28
Aleksei Miros <aleksei.miros@outlook.com>
2023-09-28 12:53:39 +1000

    Attempting 1.5

--- part1/src/App.jsx
+++ part1/src/App.jsx
@@ -1,17 +1,27 @@
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+
+
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
 
   return (
     <div>
       <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
+      <Content parts={parts}/>
+      <Total parts={parts}/>
     </div>
   )
 }
@@ -25,30 +35,39 @@ const Header = (props) => {
 }
 
 const Content = (props) => {
+  const partElements = props.parts.map((part, index) => {
+    <div key={index}>
+      <Part part={part}/>
+    </div>
+  });
+
   return (
     <div>
-      <Part part={props.part1} exercise={props.exercises1}/>
-      <Part part={props.part2} exercise={props.exercises2}/>
-      <Part part={props.part3} exercise={props.exercises3}/>
+      {partElements}
+    </div>
+  );
+}
+
+
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>
+        {props.part.name} {props.part.exercises}
+      </p>
     </div>
   )
 }
 
 const Total = (props) => {
+  let total = 0
+  props.parts.forEach(part => {
+    total += part.exercises
+  })
   return (
     <div>
-      <p>Number of exercises {props.exercises1 + props.exercises2 + props.exercises3}</p>
-    </div>
-  )
-}
-
-const Part = (props) => {
-
-  return (
-    <div>
-      <p>
-        {props.part} {props.exercise}
-      </p>
+      <p>Number of exercises {total}</p>
     </div>
   )
 }
