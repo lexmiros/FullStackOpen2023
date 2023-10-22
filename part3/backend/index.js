25ae043065b1ccb19238d4b489ed4efe6be9f8b2
Aleksei Miros <aleksei.miros@outlook.com>
2023-10-22 11:17:13 +1000

    Update part3 for 3.15 to 3.18

--- part3/backend/index.js
+++ part3/backend/index.js
@@ -1,105 +1,128 @@
-let phonebookData = [
-  {
-    id: 1,
-    name: "Arto Hellas",
-    number: "040-123456",
-  },
-  {
-    id: 2,
-    name: "Ada Lovelace",
-    number: "39-44-5323523",
-  },
-  {
-    id: 3,
-    name: "Dan Abramov",
-    number: "12-43-234345",
-  },
-  {
-    id: 4,
-    name: "Mary Poppendieck",
-    number: "39-23-6423122",
-  },
-];
-
-
+require("dotenv").config();
 const express = require("express");
 const morgan = require("morgan");
 const cors = require("cors");
-
+const Person = require("./models/person");
+const { default: mongoose } = require("mongoose");
 const app = express();
+const PORT = process.env.PORT;
 
-morgan.token('req-body', function (req, res) {
-  if (req.method === 'POST') {
+morgan.token("req-body", function (req, res) {
+  if (req.method === "POST") {
     return JSON.stringify(req.body);
   }
-  return '-';
+  return "-";
 });
 
-const customFormat = ':method :url :status :res[content-length] - :response-time ms :req-body';
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
 
-app.use(express.static('dist'))
-app.use(express.json());
-app.use(morgan(customFormat))
-app.use(cors())
+  if (error.name === "CastError") {
+    return response.status(400).send({error: "Incorrect ID format"})
+  }
 
+  next(error)
+}
 
-const PORT =  process.env.PORT || 3001
-
-const createNewMaxId = () => {
-    return Math.floor(Math.random() * 1000000000)
+const unknownEndpoint = (request, response) => {
+  response.status(404).send({error : "Unknown endpoint"})
 }
 
 
-app.get("/info", (request, response) => {
-    const numberOfPhonebookEntries = phonebookData.length;
-    const currentDateTime = new Date();
+const customFormat =
+  ":method :url :status :res[content-length] - :response-time ms :req-body";
 
-    response.send(
-        `Phonebook has info for ${numberOfPhonebookEntries} people <br/>
-        ${currentDateTime}
-        `
-    )
+app.use(express.static("dist"));
+app.use(express.json());
+app.use(morgan(customFormat));
+app.use(cors());
 
-})
+app.get("/info", (request, response, next) => {
+  const currentDateTime = new Date();
+  Person.find({})
+    .then(persons => {
+      response.send(`There are currently ${persons.length} people as of ${currentDateTime}`)
+    })
+    .catch(error => next(error))
+});
 
 app.get("/api/persons", (request, response) => {
-    response.json(phonebookData)
-})
+  Person.find({}).
+    then(people => {
+      response.json(people)
+    })
+    .catch(error => next(error))
+});
 
-app.get("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    const person = phonebookData.find(entry => entry.id === id)
+app.get("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id
 
+  Person.findById(request.params.id)
+  .then(person => {
     if (person) {
-        response.json(person)
+      response.json(person)
+    } else {
+      console.log(`Unable to find person with ID ${request.params.id}`)
+      response.status(404).json({error:`404 No person with id ${request.params.id} found`})
     }
-    else {
-        response.status(404).end()
-    }
+  })
+  .catch(error => next(error))
+  
+});
+
+app.delete("/api/persons/:id", (request, response, next) => {
+  const id = request.params.id
+
+  if (!isValidId(id)) {
+    console.log(`Invalid ObjectId: ${id}`);
+    response.status(400).json({ error: "Invalid ObjectId" });
+    return;
+  }
+
+  Person.findByIdAndDelete(id)
+    .then(deletedPerson => {
+      console.log(`Person with id ${id} deleted`)
+      response.status(204).end();
+    })
+    .catch(error => next(error))
+});
+
+app.post("/api/persons", (request, response, next) => {
+  const body = request.body
+
+  const person = new Person({
+    name: body.name,
+    number: body.number
+  })
+
+  person.validate()
+  .then( () => {
+    return person.save()
+  })
+  .then(savedPerson => {
+    response.status(201).json(savedPerson)
+  })
+  .catch(error => next(error))
+});
+
+app.put("/api/persons/:id", (request, response, next) => {
+  const body = request.body
+
+  const newPerson = {
+    name: body.name,
+    number: body.number
+  }
+
+  Person.findByIdAndUpdate(request.params.id, newPerson, {new: true})
+    .then(updatedPerson => {
+      response.json(updatedPerson)
+    })
+    .catch(error => next(error))
 })
 
-app.delete("/api/persons/:id", (request, response) => {
-    const id = Number(request.params.id)
-    phonebookData = phonebookData.filter(entry => entry.id !== id)
-
-    response.status(204).end()
-})
-
-app.post("/api/persons", (request, response) => {
-    const newEntry = request.body
-    const isNameUnique = phonebookData.some(entry => entry.name === newEntry.name)
-    
-    const id = createNewMaxId()
-    newEntry.id = id
-
-    phonebookData = phonebookData.concat(newEntry)
-
-    response.json(newEntry)
-
-})
-
+app.use(unknownEndpoint)
+app.use(errorHandler)
 
 app.listen(PORT, () => {
-    console.log(`Server started on port http://localhost:${PORT}`)
-})
-
+  console.log(`Server started on port http://localhost:${PORT}`);
+});
