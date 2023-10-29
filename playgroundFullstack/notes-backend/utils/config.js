4b4c8384e5c3d27209bf1f9181e01748d6626fa6
Aleksei Miros <aleksei.miros@outlook.com>
2023-10-30 07:56:47 +1000

    Update config file to use test DB on testing

--- playgroundFullstack/notes-backend/utils/config.js
+++ playgroundFullstack/notes-backend/utils/config.js
@@ -1,7 +1,10 @@
 require("dotenv").config()
 
 const PORT = process.env.PORT
-const MONGO_DB_URI = process.env.MONGO_DB_URI
+
+const MONGO_DB_URI = process.env.NODE_ENV === "production"
+  ? process.env.MONGO_DB_URI
+  : process.env.MONGO_DB_TEST_URI
 
 module.exports = {
     PORT,
